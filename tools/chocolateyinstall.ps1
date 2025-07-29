2fdbbac1391721f8ee1f988fa9f9e2ee140bd72f
Pragadesh-45 <temporaryg7904@gmail.com>
2025-07-29 12:27:57 +0545

    Revert "Update to Bruno v2.8.1 (#11)"
    This reverts commit 585241ca78692ac10bd52e20956b2ffa6f331abd.

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,19 +1,18 @@
 $ErrorActionPreference = 'Stop'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com/usebruno/bruno/releases/download/v2.8.1/bruno_2.8.1_x64_win.exe'
+$url = 'https://github.com/usebruno/bruno/releases/download/v2.8.0/bruno_2.8.0_x64_win.exe'
 
 $packageArgs = @{
   packageName  = '$env:ChocolateyPackageName'
   fileType     = 'exe'
   url          = $url
   softwareName = 'Bruno*'
-  checksum     = '43E3FF6A49C9D50210DD33124662987049C7618E7567AE4E5BC81333ECDED429'
+  checksum     = '473460988BF8823C3E513E2B957832359C61DF31FD4485F847B8DBBD03447264'
   checksumType = 'sha256'
   silentArgs   = '/S'
 }
 
 Install-ChocolateyPackage @packageArgs
-
 
 
 
