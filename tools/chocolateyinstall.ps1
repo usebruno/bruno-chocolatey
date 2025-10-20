fc247d1c3d68841c8c4c2cb13603e84d3bea21c9
Pragadesh-45 <temporaryg7904@gmail.com>
2025-10-20 17:24:28 +0545

    feat: modify silentArgs to include '/allusers'

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -9,7 +9,7 @@ $packageArgs = @{
   softwareName = 'Bruno*'
   checksum     = '65F7E8ACDF4AB95644E6867EA7AC9EFFF6520EB31D86379F4F82F6B5B2A06085'
   checksumType = 'sha256'
-  silentArgs   = '/S'
+  silentArgs   = '/S /allusers'
 }
 
 Install-ChocolateyPackage @packageArgs
