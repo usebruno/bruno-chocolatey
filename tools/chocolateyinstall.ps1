803a737f6e63a405630a4ece7103c4c9ba841ef4
Pragadesh44-Bruno <Pragadesh44-Bruno@users.noreply.github.com>
2025-11-10 16:50:57 +0000

    Update to Bruno v2.14.1

--- tools/chocolateyinstall.ps1
+++ tools/chocolateyinstall.ps1
@@ -1,18 +1,19 @@
 $ErrorActionPreference = 'Stop'
 $toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com/usebruno/bruno/releases/download/v2.14.0/bruno_2.14.0_x64_win.exe'
+$url = 'https://github.com/usebruno/bruno/releases/download/v2.14.1/bruno_2.14.1_x64_win.exe'
 
 $packageArgs = @{
   packageName  = '$env:ChocolateyPackageName'
   fileType     = 'exe'
   url          = $url
   softwareName = 'Bruno*'
-  checksum     = 'BDE411165FDBD148C7668FE545B667F65EC583DB90A6D3CD9BC70456FE5F9346'
+  checksum     = '9DF1BD6681568F7E88DEB4D285DF6C4170060BCF95BBF42ABA057FCC48C6D889'
   checksumType = 'sha256'
   silentArgs   = '/S /allusers'
 }
 
 Install-ChocolateyPackage @packageArgs
+
 
 
 
